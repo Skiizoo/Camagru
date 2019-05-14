fb2b64f9bf2d80588ae258264336ab9060efc233
BROGGI Thomas <tbroggi@student.42.fr>
2019-05-14 13:10:46 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
-    $DB_HOST = '172.18.0.2';
-    $DB_PORT = '3306';
-    $DB_DATABASE = 'ddb_camagru';
+    $DB_HOST = 'XXXXXX';
+    $DB_PORT = 'XXXXXX';
+    $DB_DATABASE = 'XXXXXX';
     $DB_DSN = 'mysql:host=' . $DB_HOST . ';dbname=' . $DB_DATABASE . ';port=' . $DB_PORT;
-    $DB_USERNAME = 'tbroggi';
-    $DB_PASSWORD = 'tbroggi';
+    $DB_USERNAME = 'XXXXXX';
+    $DB_PASSWORD = 'XXXXXX';
